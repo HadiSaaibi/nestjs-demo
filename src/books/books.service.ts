6b72708e82c1b4b52e06f6a5d5218342dc37ea66
HadiSaaibi <90275015+HadiSaaibi@users.noreply.github.com>
2026-05-22 09:41:21 +0300

    chore: add placeholder data

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -4,7 +4,21 @@ import { AuthorsService } from '../authors/authors.service';
 @Injectable()
 export class BooksService {
   private books = [
-    // TODO: add some sample books here
+      {
+        id: 1,
+        title: "Harry and the Sorcerer's Stone",
+        authorId: 1,
+    },
+      {
+        id: 2,
+        title: 'Harry Potter and the Chamber of Secrets',
+        authorId: 1,
+    },
+      {
+        id: 3,
+        title: 'How to make friends and influence people',
+        authorId: 2,
+      }
   ];
 
   constructor(private readonly authorsService: AuthorsService) {}
